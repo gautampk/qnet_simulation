89beb18dba919fb8185d0c3f7b88f7e1d56cd201
qs-gautampk <gautam.kambhampati@quantspark.com>
2018-05-01 16:27:02 +0100

    Add basic networkx functionality

--- qnets/qnet.py
+++ qnets/qnet.py
@@ -3,6 +3,8 @@ import itertools
 
 # Third party modules
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt 
 
 # My modules
 from .utils import entangled
@@ -39,7 +41,27 @@ class qnet:
                 if qubit == a:
                     return node
 
-    def _edges(self):
+    def draw(self):
+        nx.draw(self._graph(), with_labels=True, font_weight='bold')
+        plt.show()
+
+    def _graph(self):
+        # Create graph object.
+        graph = nx.Graph()
+
+        # Add nodes with list of qubits as an attribute.
+        for node in self.nodes:
+            graph.add_node(node, qubits=self.nodes[node])
+
+        # Generate edges.
+        graph.add_edges_from(self._getEdges())
+
+        # Return.
+        return graph
+
+    def _getEdges(self):
+        # Find those qubits which are entagled using the Peres-Horodecki
+        #   criterion.
         entagledPairs = []
         for a, b in itertools.product(self.qubits, self.qubits):
             if entangled(self.state.ptrace([self.qubits.index(a), self.qubits.index(b)])):
@@ -50,10 +72,17 @@ class qnet:
         for pair in entagledPairs:
             edges.append((self.nodeOf(pair[0]), self.nodeOf(pair[1])))
 
+        # Return.
         return edges
 
     def __getattr__(self, attr):
-        if attr == 'edges':
-            return self._edges()
+        if attr == 'graph':
+            return self._graph()
+        elif attr == 'edges':
+            return list(self._graph().edges)
+        elif attr == 'order':
+            return len(self._graph().nodes)
+        elif attr == 'size':
+            return len(self._graph().edges)
         else:
             raise AttributeError('No such attribute.')
